676dedccb177fed99d81f45ec851bca4916bc4c0
seven272 <i727i@mail.ru>
2023-11-04 17:40:20 +0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>book library app</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
