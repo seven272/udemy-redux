8840a2db41a8833fac14b25bf1d6736b8f67da4d
seven272 <i727i@mail.ru>
2023-11-07 15:29:45 +0300

    upgrate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import BookList from './components/BookList/BookList'
 
 function App() {
   return (
+    
     <div className="app">
       <header className="app-header">
         <h1>book library app</h1>
@@ -19,6 +20,7 @@ function App() {
         </div>
       </main>
     </div>
+  
   )
 }
 
---
+++ frontend/src/redux/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+const store = configureStore({
+    reducer: {},
+})
+console.log(store)
+export default store
